8a4295ef9a8133d74444928303fb89f4108a1418
Robert Krussel <bkrus17@gmail.com>
2017-10-19 23:37:23 -0500

    getting closer

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogx:blogx@localhost:8889/blogx'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'randomkey'
@@ -17,7 +17,7 @@ class Blog (db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
         self.owner = owner
@@ -27,7 +27,7 @@ class User (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(50))
-    #blog_id = db.relationship('id', backref = 'owner') 
+    blogs = db.relationship('Blog', backref = 'owner') 
 
     def __init__(self, username, password):
         self.username = username
@@ -39,6 +39,12 @@ def is_empty (n):
         if n == "":
             return True
 
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'signup']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('/login')
+
 @app.route ('/login', methods=['POST','GET'])
 def login():
     if request.method == 'POST':
@@ -46,11 +52,12 @@ def login():
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
         if user and user.password == password:
-            #TODO remember user has logged in
-            return redirect('/blog')
+            session['username'] = username
+            flash ("Logged in!", "info")
+            return redirect('/newpost')
         else:
-            #TODO explain why login failed
-            return '<h1>Error</h1>'
+            flash ('Username or Password is incorrect', 'error')
+            return redirect('/login')
     return render_template('login.html')
 
 @app.route ('/signup', methods=['POST','GET'])
@@ -60,20 +67,23 @@ def signup():
         password = request.form['password']
         verify = request.form['verify']
 
-        #TODO - add in validations
-
         existing_user = User.query.filter_by(username=username).first()
         if not existing_user:
             new_user = User(username, password)
             db.session.add(new_user)
             db.session.commit()
-            # TODO remember user logged in
+            session['username'] = username
             return redirect ('/blog')
         else:
-            #TODO - user exists message
-            return '<h1>dupe user</h1>'        
+            flash ('Username is taken', 'error')
+            return redirect('/signup')        
 
     return render_template('signup.html')
+
+@app.route ('/logout')
+def logout():
+    del session['username']
+    return redirect ('/blog')
 
 #displays main page with all blog entries
 @app.route('/blog', methods = ['POST', 'GET'])
@@ -84,14 +94,16 @@ def index():
     #queries for a single blog entry based on blog_id
     single = Blog.query.filter_by(id = blog_id).first()
     
+    #queries for logged in user. 
+    owner = User.query.filter_by(username=session['username']).first()
+    
     #sends user to single view if request.args is present
     if request.args: 
         return render_template('single_view.html', title=single.title, body=single.body)   
     
     #else, takes user to main page
-    blogs = Blog.query.all()
+    blogs = Blog.query.filter_by(owner=owner).all()
     return render_template('blog.html', title="Blog Entry", blogs=blogs)
-
 
 
 #displays form for new blog entry
@@ -103,12 +115,11 @@ def display_newpost():
 @app.route('/newpost', methods = ['POST'])    #Think about what needs to change here now that we have user sign in. 
 def newpost():                                #Will need to incorperate owner_id
     title_name = request.form ['title']
-    title_error = ''
-    
     body_text = request.form ['body']
-    body_error = ''
+    owner = User.query.filter_by(username=session['username']).first()
     
-    #validate input fields are not empty
+    
+    #TODO add FLASH validate input fields are not empty
     if is_empty(title_name): 
         title_error = 'Title cannot be empty'
 
@@ -123,7 +134,7 @@ def newpost():                                #Will need to incorperate owner_id
         
     #if no errors, save to the DB
     else:
-        new_blog = Blog(title_name, body_text)
+        new_blog = Blog(title_name, body_text, owner)
         db.session.add(new_blog)
         db.session.commit()
         
